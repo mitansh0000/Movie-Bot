de2cb0c49a328196eccbcf80f610f723ae4b9fb9
Mitansh Dhiman <118064821+mitansh0000@users.noreply.github.com>
2024-01-25 11:55:35 +0530

    Update url_shortner.py

--- plugins/josprojects/url_shortner.py
+++ plugins/josprojects/url_shortner.py
@@ -23,7 +23,7 @@ from pyshorteners import Shortener
 BITLY_API = os.environ.get("BITLY_API", "8df1df8c23f719e5cf97788cc2d40321ea30092b")
 CUTTLY_API = os.environ.get("CUTTLY_API", "f64dffbde033b6c307387dd50b7c76e505f1c")
 SHORTCM_API = os.environ.get("SHORTCM_API", "pk_...NIZv")
-GPLINKS_API = os.environ.get("GPLINKS_API", "008ccaedd6061ad1948838f410947603de9007a7")
+GPLINKS_API = os.environ.get("GPLINKS_API", "4fb7fc265581e5d328cd58499762060e5ab4a84b")
 
 reply_markup = InlineKeyboardMarkup(
         [[
@@ -190,7 +190,7 @@ async def short(link):
     
     # Send the text
     try:
-        shorten_urls += "\n\nmade by @josprojects | @jospsupport 🔥"
+        shorten_urls += "\n\nmade by @theprofffesorr | @theprofffesorr 🔥"
         return shorten_urls
     except Exception as error:
         return error
